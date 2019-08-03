b785a6b161ab2c34d155d2d40ee3a68567f84e13
subbu <subbu@highpeaksw.com>
2019-08-04 02:00:56 +0530

    services

--- dao.js
+++ dao.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'postgres',
   host: '134.209.155.59',
-//  host: '192.168.0.12',
+//  host: '192.168.0.12', local testing
   database: 'postgres',
   password: 'psql123',
   port: 5432
